2e7ed112e44649f7a528444e8f614f916f4ee9f7
Ivar Nilsen <post@ivarnilsen.com>
2016-01-16 22:10:22 +0100

    Use spaces

--- init.el
+++ init.el
@@ -5,7 +5,7 @@
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
 
-;;theme
+;;solarized dark theme
 (setq custom-theme-directory (concat user-emacs-directory "themes"))
 (dolist
     (path (directory-files custom-theme-directory t "\\w+"))
@@ -14,3 +14,6 @@
 (load-theme 'solarized t)
 (set-frame-parameter nil 'background-mode 'dark)
 (enable-theme 'solarized)
+
+;;use spaces
+(setq-default indent-tabs-mode nil)
