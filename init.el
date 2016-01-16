f82b3aa93d946eb84ab59bfe15f424c0b6eb6fc7
Ivar Nilsen <post@ivarnilsen.com>
2016-01-16 21:53:09 +0100

    Add solarized theme

--- init.el
+++ init.el
@@ -4,3 +4,13 @@
 ;;start in fullscreen
 (custom-set-variables
  '(initial-frame-alist (quote ((fullscreen . maximized)))))
+
+;;theme
+(setq custom-theme-directory (concat user-emacs-directory "themes"))
+(dolist
+    (path (directory-files custom-theme-directory t "\\w+"))
+  (when (file-directory-p path)
+    (add-to-list 'custom-theme-load-path path)))
+(load-theme 'solarized t)
+(set-frame-parameter nil 'background-mode 'dark)
+(enable-theme 'solarized)
