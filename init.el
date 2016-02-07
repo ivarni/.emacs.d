2713992448de27de33f6b54240912bd0ae783d4c
Ivar Nilsen <post@ivarnilsen.com>
2016-02-07 17:23:59 +0100

    Remember cursor position

--- init.el
+++ init.el
@@ -23,3 +23,7 @@
 
 ;;add newline at end of files
 (setq require-final-newline t)
+
+(require 'saveplace)                          ;; get the package
+(setq save-place-file "~/.emacs.d/saveplace") ;; keep my ~/ clean
+(setq-default save-place t)                   ;; activate it for all buffers
